ea67c0a5531bd390bb02014b338955881b45a156
kr1s7ian <kristianboy44@gmail.com>
2022-10-01 05:14:46 +0200

    made main.rs cleaner by using the new ComponentHandler struct

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,28 @@ extern crate olc_pixel_game_engine;
 
 mod gof;
 mod common;
+mod component_handler;
+use std::ops::Deref;
+
 use common::Component;
 use gof::Gof;
+use component_handler::ComponentHandler;
 use crate::olc_pixel_game_engine as olc;
 
 struct Game {
-  pub gof: Gof,
-  pub timer: f32,
+  pub component_handler: ComponentHandler,
 }
 
 impl olc::Application for Game {
   fn on_user_create(&mut self) -> Result<(), olc::Error> {
+    self.component_handler.add_component(Gof::new());
     Ok(())
   }
 
   fn on_user_update(&mut self, elapsed_time: f32) -> Result<(), olc::Error> {
-    olc::clear(olc::Pixel::rgb(255u8, 255u8, 255u8));
-    self.gof.update(elapsed_time);
-    self.gof.draw();
+    //olc::clear(olc::Pixel::rgba(255u8, 255u8, 255u8, 255u8));
+    self.component_handler.update(elapsed_time);
+    self.component_handler.draw();
     Ok(())
   }
 
@@ -29,8 +33,7 @@ impl olc::Application for Game {
 }
 fn main() {
   let mut game = Game {
-    gof: Gof::new(),
-    timer: 0.0
+    component_handler: ComponentHandler::new(),
   };
-  olc::start("Hello, World!", &mut game, 150, 150, 1, 1).unwrap();
+  olc::start("Hello, World!", &mut game, 100, 100, 1, 1).unwrap();
 }
