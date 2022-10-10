d3d59e552f4ab34e70c77d7062eaef850642539d
kr1s7ian <kristianboy44@gmail.com>
2022-10-10 11:51:41 +0200

    added cell enum with Dead and Alive variant, made code cleaner and more self explainatory

--- src/gof.rs
+++ src/gof.rs
@@ -1,10 +1,49 @@
 use crate::common::{Component, Vec2};
-use crate::{olc_pixel_game_engine as olc, Game};
+use crate::olc_pixel_game_engine as olc;
+
 use rand::Rng;
+use rand::distributions::Standard;
+use rand::prelude::Distribution;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum Cell{
+    Alive,
+    Dead,
+}
+
+impl Cell {
+    pub fn as_u8(&self) -> u8 {
+        match self {
+            Cell::Alive => return 1,
+            Cell::Dead => return 0,
+        }
+    }
+
+    pub fn is_alive(&self) -> bool {
+        match self {
+            Cell::Alive => return true,
+            Cell::Dead => return false,
+        }
+    }
+
+    pub fn is_dead(&self) -> bool {
+        return ! self.is_alive()
+    }
+}
+
+impl Distribution<Cell> for Standard {
+    fn sample<R: Rng + ?Sized> (&self, rng: &mut R) -> Cell {
+        match rng.gen_range(0..=1) {
+            0 => Cell::Dead,
+            1 => Cell::Alive,
+            _ => Cell::Alive,
+        }
+    }
+}
 
 #[derive(Clone)]
 pub struct Gof {
-    content: Vec<u8>,
+    content: Vec<Cell>,
     dimensions: Vec2<usize>,
     offset: Vec2<f32>,
     zoom: i32,
@@ -13,79 +52,79 @@ pub struct Gof {
 
 impl Gof {
     pub fn new() -> Self {
-        let height: usize = (olc::screen_height()) as usize;
-        let width: usize = (olc::screen_width()) as usize;
-        let size: usize = width * height;
+        let height: usize = olc::screen_height() as usize;
+        let width: usize = olc::screen_width() as usize;
+        let vec_length: usize = width * height;
+
         let mut result = Self {
             dimensions: Vec2 {
                 x: width,
                 y: height,
             },
-            content: vec![0u8; size],
+            content: vec![Cell::Dead; vec_length],
             offset: Vec2 { x: 0.0, y: 0.0 },
             zoom: 1,
             running: true,
         };
-        result.start();
+        result.init();
+
         result
     }
 
-    pub fn randomize(&mut self) {
+    pub fn get_cell(&self, x: usize, y: usize) -> Cell {
+        let i: usize = y * self.dimensions.x + x;
+        return self.content[i];
+    }
+
+    pub fn set_cell(&mut self, x: usize, y: usize, value: Cell){
+        let i: usize = y * self.dimensions.x + x;
+        self.content[i] = value;
+    }
+
+    pub fn fill_cells(&mut self, state: Cell) {
+        self.content.iter_mut().for_each(|f| *f = state);
+    }
+
+    pub fn randomize_cells(&mut self) {
         let mut rng = rand::thread_rng();
-        let mut state = false;
         for cell in self.content.iter_mut() {
-            state = rng.gen();
-            *cell = state as u8;
+            let random_state = rng.gen();
+            *cell = random_state;
         }
     }
 
-    pub fn clear(&mut self) {
-        self.content.iter_mut().for_each(|f| *f = 0);
-    }
-
-    pub fn get_cell(&self, x: usize, y: usize) -> &u8 {
-        let i: usize = y * self.dimensions.x + x;
-        return &self.content[i];
-    }
-
-    pub fn set_cell(&mut self, x: usize, y: usize, value: u8) -> &u8 {
-        let i: usize = y * self.dimensions.x + x;
-        self.content[i] = value;
-        return &self.content[i];
-    }
-
-    pub fn neighbors(&self, x: usize, y: usize) -> u8 {
-        let mut result = 0u8;
+    pub fn cell_neighbors(&self, x: usize, y: usize) -> u8 {
+        let mut alive_neighbors = 0u8;
 
         for i in 0..3 {
             for j in 0..3 {
                 let x = x - 1 + i;
                 let y = y - 1 + j;
                 let cell = self.get_cell(x, y);
-                result += cell;
+                alive_neighbors += cell.as_u8();
             }
         }
 
-        result - self.get_cell(x, y)
+        alive_neighbors - self.get_cell(x, y).as_u8()
     }
 
-    pub fn tick(&mut self) {
-        let mut new = self.clone();
+    pub fn advance_generation(&mut self) {
+        let mut next_gen = self.clone();
         for x in 1..self.dimensions.x - 1 {
             for y in 1..self.dimensions.y - 1 {
+                let neighbors = self.cell_neighbors(x, y);
                 let cell = self.get_cell(x, y);
-                let neighbors = self.neighbors(x, y);
 
-                if *cell == 0u8 && neighbors == 3 {
-                    new.set_cell(x, y, 1);
-                } else if *cell == 1u8 && (neighbors < 2 || neighbors > 3) {
-                    new.set_cell(x, y, 0);
+                if cell.is_dead() && neighbors == 3 {
+                    next_gen.set_cell(x, y, Cell::Alive);
+                } else if cell.is_alive() && (neighbors < 2 || neighbors > 3) {
+                    next_gen.set_cell(x, y, Cell::Dead);
                 } else {
-                    new.set_cell(x, y, *cell);
+                    next_gen.set_cell(x, y, cell);
                 }
             }
         }
-        self.content = new.content;
+        self.content = next_gen.content;
     }
 
     #[inline]
@@ -98,13 +137,9 @@ impl Gof {
 }
 
 impl Component for Gof {
-    fn id(&self) -> Option<u32> {
-        Some(0u32)
-    }
 
-    fn start(&mut self) {
-        self.randomize();
-        self.clear();
+    fn init(&mut self) {
+        self.randomize_cells();
     }
 
     fn poll_inputs(&mut self, elapsed_time: f32) {
@@ -115,17 +150,17 @@ impl Component for Gof {
 
         // randomize cells when R is pressed
         if olc::get_key(olc::Key::R).pressed {
-            self.randomize();
+            self.randomize_cells();
         }
 
         // set every cell to 0 when C is pressed
         if olc::get_key(olc::Key::C).pressed {
-            self.clear();
+            self.fill_cells(Cell::Dead);
         }
 
         // simulate 1 frame if RIGHT is pressed
         if olc::get_key(olc::Key::RIGHT).pressed {
-            self.tick();
+            self.advance_generation();
         }
 
         // change zoom with SCROLLWHEEL
@@ -143,7 +178,6 @@ impl Component for Gof {
         let new_pos = self.screen_to_world_point(olc::get_mouse_x(), olc::get_mouse_y());
         let diff_x = old_pos.x - new_pos.x;
         let diff_y = old_pos.y - new_pos.y;
-        println!("x: {}, y: {}", diff_x, diff_y);
         self.offset.x -= diff_x as f32;
         self.offset.y -= diff_y as f32;
 
@@ -169,29 +203,30 @@ impl Component for Gof {
             if (coords.x > 0 && coords.x < self.dimensions.x as i32)
                 && (coords.y > 0 && coords.y < self.dimensions.y as i32)
             {
-                self.set_cell(coords.x as usize, coords.y as usize, 1u8);
+                self.set_cell(coords.x as usize, coords.y as usize, Cell::Alive);
             }
         }
     }
 
     fn update(&mut self, _elapsed_time: f32) {
-        // update sim only of the sim is not paused
         if self.running {
-            self.tick();
+            self.advance_generation();
         }
     }
 
-    fn draw(&mut self) {
+    fn draw(&self) {
+        const WHITE: olc::Pixel = olc::Pixel::rgb(255u8, 255u8,255u8);
+        const RED: olc::Pixel = olc::Pixel::rgb(255u8, 140u8,140u8);
+
         for y in 0..self.dimensions.y {
             for x in 0..self.dimensions.x {
-                let mut p = olc::Pixel::rgb(255u8, 255u8, 255u8);
-                if *self.get_cell(x as usize, y as usize) == 1 {
-                    p = olc::Pixel::rgb(255u8, 140u8, 140u8);
-                }
+                let cell = self.get_cell(x as usize, y as usize);
+                let mut color = WHITE;
+                if cell.is_alive() { color = RED }
+
                 let world_x = (x as i32 + self.offset.x as i32) * self.zoom as i32;
                 let world_y = (y as i32 + self.offset.y as i32) * self.zoom as i32;
-                olc::fill_rect(world_x as i32, world_y as i32,self.zoom as i32, self.zoom as i32, p);
-
+                olc::fill_rect(world_x as i32, world_y as i32,self.zoom as i32, self.zoom as i32, color);
             }
         }
     }
@@ -199,28 +234,46 @@ impl Component for Gof {
 
 #[allow(unused_imports)]
 mod tests {
-    use crate::gof::Gof;
+    use crate::gof::{Gof, Cell};
 
     #[test]
     fn test_neighbors() {
         let mut gof = Gof::new();
         for x in 0..3 {
             for y in 0..3 {
-                gof.set_cell(x, y, 1);
+                gof.set_cell(x, y, Cell::Alive);
             }
         }
-        let result = gof.neighbors(1, 1);
+        let result = gof.cell_neighbors(1, 1);
         assert_eq!(result, 8);
 
         for x in 0..3 {
             for y in 0..3 {
-                gof.set_cell(x, y, 0);
+                gof.set_cell(x, y, Cell::Dead);
             }
         }
         for y in 0..3 {
-            gof.set_cell(0, y, 1);
+            gof.set_cell(0, y, Cell::Alive);
         }
-        let result = gof.neighbors(1, 1);
+        let result = gof.cell_neighbors(1, 1);
         assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn test_cell_is_alive() {
+        let cell = Cell::Alive;
+        assert_eq!(cell.is_alive(), true);
+
+        let cell = Cell::Dead;
+        assert_eq!(cell.is_alive(), false);
+    }
+
+    #[test]
+    fn test_cell_is_dead() {
+        let cell = Cell::Dead;
+        assert_eq!(cell.is_dead(), true);
+
+        let cell = Cell::Alive;
+        assert_eq!(cell.is_dead(), false);
     }
 }
