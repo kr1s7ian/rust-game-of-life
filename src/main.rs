5495074f66a3e3b81d130c85441255309252f3fe
kr1s7ian <kristianboy44@gmail.com>
2022-10-10 11:48:45 +0200

    removed useless code

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 extern crate olc_pixel_game_engine;
+use crate::olc_pixel_game_engine as olc;
 
 mod gof;
 mod common;
 mod component_handler;
 
-use common::{Component, Vec2};
 use gof::Gof;
 use component_handler::ComponentHandler;
-use crate::olc_pixel_game_engine as olc;
 
 pub struct Game {
   pub component_handler: ComponentHandler,
-  pub offset: Vec2<i32>,
 }
 
 impl olc::Application for Game {
@@ -22,6 +20,7 @@ impl olc::Application for Game {
 
   fn on_user_update(&mut self, elapsed_time: f32) -> Result<(), olc::Error> {
     olc::clear(olc::Pixel::rgb(140u8, 140u8, 140u8));
+
     self.component_handler.poll_inputs(elapsed_time);
     self.component_handler.update(elapsed_time);
     self.component_handler.draw();
@@ -35,7 +34,6 @@ impl olc::Application for Game {
 fn main() {
   let mut game = Game {
     component_handler: ComponentHandler::new(),
-    offset: Vec2{x: 0, y: 0},
   };
-  olc::start("Hello, World!", &mut game, 1920/10, 1080/10, 1, 1).unwrap();
+  olc::start("rust-game-of-life", &mut game, 1920/5, 1080/5, 1, 1).unwrap();
 }
